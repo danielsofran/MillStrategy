9bb61a484299e81d06434904963a245429d08f25
daniel <daniel.sofran@stud.ubbcluj.ro>
2022-08-20 11:43:15 +0300

    structure 1

--- MillStrategy/Domain/Tabla.cs
+++ MillStrategy/Domain/Tabla.cs
@@ -18,6 +18,8 @@ namespace MillStrategy.Domain
         public PiesaColor PlayerColor { get; set; } = PiesaColor.None;
         public bool IsBlack { get => PlayerColor == PiesaColor.Black; set => PlayerColor = value == true ? PiesaColor.Black : PiesaColor.None; }
         public bool IsWhite { get => PlayerColor == PiesaColor.White; set => PlayerColor = value == true ? PiesaColor.White : PiesaColor.None; }
+        public bool Ocupata { get => PlayerColor != PiesaColor.None; }
+        public bool Libera { get => PlayerColor == PiesaColor.None; set => PlayerColor = PiesaColor.None; }
         public PiesaState State { get; set; }
     }
 
@@ -27,14 +29,7 @@ namespace MillStrategy.Domain
         {
             Nume = nume;
             PiesaColor = piesaColor;
-            List<IPiesa> depus = new List<IPiesa>();
-            for(int i=0;i<9;++i)
-            {
-                Piesa p = new Piesa(PiesaColor, PiesaState.Nepusa);
-                depus.Add(p);
-            }
-            DePus = depus;
-            Luate = new List<IPiesa>();
+            Reset();
         }
 
         public string Nume { get; set; } = "Player";
@@ -43,6 +38,91 @@ namespace MillStrategy.Domain
         public List<IPiesa> DePus { get; set; }
         public List<IPiesa> Luate { get; set; }
 
-        public 
+        public void Reset()
+        {
+            List<IPiesa> depus = new List<IPiesa>();
+            for (int i = 0; i < 9; ++i)
+            {
+                Piesa p = new Piesa(PiesaColor, PiesaState.Nepusa);
+                depus.Add(p);
+            }
+            DePus = depus;
+            Luate = new List<IPiesa>();
+        }
+        public IPiesa Pune()
+        {
+            var piesa = DePus.Last();
+            DePus.RemoveAt(DePus.Count - 1);
+            return piesa;
+        }
+        public void Ia(IPiesa piesa)
+        {
+            Luate.Add(piesa);
+        }
+    }
+
+    public class Tabla : ITabla
+    {
+        IPlayer player = new Player();
+
+        public Tabla()
+        {
+            Player1 = new Player(PiesaColor.White);
+            Player2 = new Player(PiesaColor.Black);
+            Player = Player1;
+
+        }
+
+        public event EventHandler PlayerChanged;
+
+        public bool SePunPiese { get => Player1.State == PlayerState.Pune || Player2.State == PlayerState.Pune; }
+        public IPlayer Player1 { get; set; }
+        public IPlayer Player2 { get; set; }
+        public IPlayer Player { get => player; 
+            set
+            {
+                player = value;
+                OnPlayerChanged(new EventArgs());
+            }
+        }
+        protected virtual void OnPlayerChanged(EventArgs e = null)
+        {
+            PlayerChanged?.Invoke(this, e);
+        }
+
+        public IPozitii PozitiiValide { get => valpoz; }
+        public Dictionary<IPozitie, IPiesa> Piese { get; set; }
+
+        public void ChangePlayer()
+        {   
+                 if(Player == Player1) Player = Player2;
+            else if(Player == Player2) Player = Player1;
+        }
+    }
+
+    public class Pozitie : IPozitie
+    {
+        OctalNumber cod;
+
+        public Pozitie(OctalNumber number)
+        {
+            cod = number;
+        }
+
+        public override OctalNumber Cod => cod;
+        public override int Square => cod.FirstDigit;
+        public override bool IsDiagonal => OctalNumber.DigitPredicates.Odd(cod.LastDigit);
+        public override bool IsLine => OctalNumber.DigitPredicates.Even(cod.LastDigit);
+
+        public override string ToString() => cod;
+        public override void FromString(string pozitie) => cod = pozitie;
+
+        public override List<OctalNumber> Vecini
+        {
+            get
+            {
+
+            }
+        }
     }
 }
--- MillStrategy/Structure/Structure.cs
+++ MillStrategy/Structure/Structure.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
-using static MillStrategy.Utils;
+using System.Collections;
 
 namespace MillStrategy.Structure
 {
@@ -11,23 +11,31 @@ namespace MillStrategy.Structure
     public enum PiesaState { Nepusa, Pusa, Luata };
     public enum PlayerState { Pune, Ia, Muta };
 
-    public interface IPozitie
+    public abstract class IPozitie
     {
-        OctalNumber Cod { get; set; }
-        List<OctalNumber> Vecini { get; }
-        int Square { get; set; }
-        bool IsDiagonal { get; }
-        bool IsLine { get; }
+        public abstract OctalNumber Cod { get; }
+        public abstract List<OctalNumber> Vecini { get; }
+        public abstract int Square { get; }
+        public abstract bool IsDiagonal { get; }
+        public abstract bool IsLine { get; }
+        public abstract override string ToString();
+        public abstract void FromString(string pozitie);
     }
 
-    public interface IPozitiePeTabla : IPozitie
+    public abstract class IPozitii : IEnumerable<IPozitie>
     {
-        bool Ocupat { get; set; }
-        bool Liber { get; set; }
+        List<IPozitie> Pozitii { get; set; }
+        public abstract override string ToString();
+        public abstract void FromString(string pozitii);
+
+        public IEnumerator<IPozitie> GetEnumerator() => Pozitii.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
     public interface IPiesa
     {
+        bool Ocupata { get; }
+        bool Libera { get; set; }
         bool IsBlack { get; set; }
         bool IsWhite { get; set; }
         PiesaColor PlayerColor { get; set; }
@@ -40,17 +48,24 @@ namespace MillStrategy.Structure
         PlayerState State { get; set; }
         List<IPiesa> DePus { get; set; }
         List<IPiesa> Luate { get; set; }
+
+        void Reset(); // reseteaza piesele
+        IPiesa Pune();
+        void Ia(IPiesa piesa);
     }
     
     public interface ITabla
     {
+        event EventHandler PlayerChanged;
+
         bool SePunPiese { get; } // daca inca se pun piese sau se muta
         IPlayer Player1 { get; set; }
         IPlayer Player2 { get; set; }
+        IPlayer Player { get; set; }
 
-        List<IPozitie> PozitiiValide { get; } // toate pozitiile disponibile
+        IPozitii PozitiiValide { get; } // toate pozitiile disponibile
         Dictionary<IPozitie, IPiesa> Piese { get; set; }
 
-        bool Ocupata(IPozitie pozitie);
+        void ChangePlayer();
     }
 }
